ac629788a323b6d423e34a98eccb44c29bf1bab8
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-07-02 18:47:29 -0400

    second-commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import moment from "moment";
 
+import PortfolioContainer from "./portfolioContainer";
+
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Thomas DiGregorios Portfolio</h1>
+        <PortfolioContainer />
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
