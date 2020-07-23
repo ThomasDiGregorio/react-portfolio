c0f79db9031d3cf0bd51844b5d56e5ccb121a4a4
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-07-23 13:17:55 -0400

    sixth commit

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolioContainer";
+import NavigationContainer from "./navigation/navigationContainer";
+
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
@@ -12,18 +15,18 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
+            <h1>Thomas DiGregorios Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
 
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
             </Switch>
           </div>
         </Router>
-
-        <h1>Thomas DiGregorios Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
       </div>
     );
   }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import PortfolioContainer from "../portfolio/portfolioContainer";
+
 export default function () {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <PortfolioContainer />
+    </div>
+  );
 }
