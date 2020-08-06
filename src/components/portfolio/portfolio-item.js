9ba893ddaf712c8b2ef482e4e961fdf40065ed92
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-08-06 13:58:09 -0400

    ninteenth commit

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -8,19 +8,23 @@ export default function (props) {
   // - description: description
   // - id: id
 
-  const { id, description, thumb_image_url, logo } = props.item;
+  const { id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div className="portfolio-item-wrapper">
       <div
         className="portfolio-img-background"
         style={{
-          backgroundImage: "url(" + thumb_image_url + ")"
+          backgroundImage: "url(" + thumb_image_url + ")",
         }}
       />
-      
-      <img src={logo} />
-      <div>{description}</div>
-      <Link to={`/portfolio/${id}`}>Link</Link>
+
+      <div className="img-text-wrapper">
+        <div classname="logo-wrapper">
+          <img src={logo_url} />
+        </div>
+
+        <div className="subtitle">{description}</div>
+      </div>
     </div>
   );
 }
