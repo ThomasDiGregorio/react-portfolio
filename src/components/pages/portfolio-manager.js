22394143f9fe9e5aa213508eb49f3a90c03ab3de
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-09-25 16:45:06 -0400

    40th commit

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -11,21 +11,23 @@ export default class PortfolioManager extends Component {
   }
 
   getPortfolioItems() {
-    axios.get("https://thomas.devcamp.space/portfolio/portfolio_items", {
-      withCredentials: true,
-    }).then(response => {
+    axios
+      .get("https://thomas.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then((response) => {
         this.setState({
-            portfolioItems: [...response.data.portfolio_items]
-        })
-    })
+          portfolioItems: [...response.data.portfolio_items],
+        });
+      })
 
-    .catch(error => {
-       console.log("error in getPortfolioItems", error); 
-    })
+      .catch((error) => {
+        console.log("error in getPortfolioItems", error);
+      });
   }
 
   componentDidMount() {
-      this.getPortfolioItems();
+    this.getPortfolioItems();
   }
 
   render() {
